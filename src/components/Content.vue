a381c7c4c435fbbf7d678742d06a7d0d5db97c2d
Alireza Tanzifian <alireza.tanzifian7@gmail.com>
2024-01-10 12:44:30 +0330

    change scroll color and fix add task

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -24,12 +24,12 @@
         </div>
       </div>
       <ADivider class="!my-2 bg-purple-300" />
-      <div class="h-48 flex flex-col gap-4 overflow-y-auto px-4">
+      <div class="h-48 flex flex-col gap-4 overflow-y-auto px-4" id="todos">
         <ToDoItem v-for="item in useTodo.todos" :data="item" />
       </div>
     </div>
     <div class="absolute w-full left-0 right-0 bottom-4 px-4">
-      <Input />
+      <Input @add="addTodo" />
     </div>
   </Card>
 </template>
@@ -43,6 +43,20 @@ import ToDoItem from './ToDoItem.vue'
 import { useTodoStore } from '../store/todos.ts'
 
 const useTodo = useTodoStore()
+
+const addTodo = (text: string) => {
+  console.log(text, 'this')
+  const newTodo = {
+    id: useTodo.nextId,
+    title: text,
+    is_finished: false
+  }
+  useTodo.addTodo(newTodo)
+  setTimeout(() => {
+    let todos = document.getElementById('todos')
+    todos && todos.scrollBy(0, document.body.scrollHeight)
+  }, 50)
+}
 </script>
 
 <style></style>
--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex gap-4 bg-white border-4 border-solid border-purple-500 rounded-full p-1">
     <input
-      class="w-full h-10 border-none outline-none rounded-xl px-4"
+      class="w-full h-10 border-none outline-none rounded-xl px-4 text-base font-medium"
       v-model="text"
-      @keyup.enter="addTodo"
+      @keyup.enter="addHandler"
       placeholder="New Task"
     />
     <button
       class="flex justify-center items-center w-12 rounded-full bg-purple-500"
-      @click="addTodo"
+      @click="addHandler"
     >
       <i class="fa fa-plus text-white" aria-hidden="true"></i>
     </button>
@@ -17,18 +17,15 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useTodoStore } from '../store/todos.ts'
 
 const text = ref('')
-const useTodo = useTodoStore()
 
-const addTodo = () => {
-  const newTodo = {
-    id: useTodo.nextId,
-    title: text.value,
-    is_finished: false
-  }
-  useTodo.addTodo(newTodo)
+const emit = defineEmits<{
+  (e: 'add', value: string): void
+}>()
+
+const addHandler = () => {
+  emit('add', text.value)
   text.value = ''
 }
 </script>
